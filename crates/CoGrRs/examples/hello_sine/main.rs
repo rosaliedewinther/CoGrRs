011ea43dd02595e50b31265238ae7495220ecfcd
jasperdewinther <jaspersophie10@gmail.com>
2022-12-19 22:58:07 +0100

    added sine example

---
+++ crates/CoGrRs/examples/hello_sine/main.rs
@@ -0,0 +1,62 @@
+use bytemuck::{Pod, Zeroable};
+use gpu::wgpu::TextureFormat::Rgba8Unorm;
+use gpu::Context;
+use gpu::Execution::PerPixel2D;
+use ui::MainGui;
+use window::{
+    input::{button::ButtonState, Input},
+    main_loop::{main_loop_run, Game, RenderResult, UpdateResult},
+    winit::window::Window,
+};
+
+pub struct HelloSine {
+    pub gpu_context: Context,
+    pub ui: MainGui,
+    pub time: f32,
+}
+
+#[repr(C)]
+#[derive(Pod, Copy, Clone, Zeroable)]
+struct GpuData {
+    time: f32,
+}
+
+impl Game for HelloSine {
+    fn on_init(window: &Window) -> Self {
+        let mut gpu_context = Context::new(window, "to_draw_texture", "examples/hello_sine/");
+
+        gpu_context.texture("to_draw_texture", (1280, 720, 1), Rgba8Unorm);
+        gpu_context.pipeline("sine", [], PerPixel2D);
+
+        let ui = MainGui::new(&gpu_context, window);
+
+        HelloSine {
+            gpu_context,
+            ui,
+            time: 0f32,
+        }
+    }
+
+    fn on_render(&mut self, _input: &mut Input, dt: f32, _window: &Window) -> RenderResult {
+        let mut encoder = self.gpu_context.get_encoder_for_draw();
+
+        self.time += dt;
+        let gpu_data = GpuData { time: self.time };
+        self.gpu_context
+            .dispatch_pipeline("sine", &mut encoder, &gpu_data);
+        self.gpu_context.image_buffer_to_screen(&mut encoder);
+
+        self.gpu_context.execute_encoder(encoder);
+        RenderResult::Continue
+    }
+
+    fn on_resize(&mut self, _new_size: (u32, u32)) {}
+
+    fn on_tick(&mut self, _dt: f32) -> UpdateResult {
+        UpdateResult::Continue
+    }
+}
+
+fn main() {
+    main_loop_run::<HelloSine>(1280, 720, 10f32);
+}
--- crates/CoGrRs/examples/hello_world/main.rs
+++ crates/CoGrRs/examples/hello_world/main.rs
@@ -1,44 +1,37 @@
-use gpu::gpu_context::GpuContext;
 use gpu::wgpu::TextureFormat::Rgba8Unorm;
-use ui::main_gui::MainGui;
+use gpu::Context;
+use ui::MainGui;
 use window::{
     input::{button::ButtonState, Input},
     main_loop::{main_loop_run, Game, RenderResult, UpdateResult},
-    winit::{dpi::PhysicalSize, window::Window},
+    winit::window::Window,
 };
 
 pub struct HelloWorld {
-    pub gpu_context: GpuContext,
+    pub gpu_context: Context,
     pub ui: MainGui,
 }
 
 impl Game for HelloWorld {
     fn on_init(window: &Window) -> Self {
-        let mut gpu_context = GpuContext::new(window, "to_draw_texture");
+        let mut gpu_context = Context::new(window, "to_draw_texture", "");
 
         gpu_context.texture("to_draw_texture", (1280, 720, 1), Rgba8Unorm);
 
-        let ui = MainGui::new(
-            &gpu_context.device,
-            &gpu_context.config.format,
-            window,
-            &gpu_context.queue,
-        );
+        let ui = MainGui::new(&gpu_context, window);
 
         HelloWorld { gpu_context, ui }
     }
 
     fn on_render(&mut self, input: &mut Input, dt: f32, window: &Window) -> RenderResult {
         let mut encoder = self.gpu_context.get_encoder_for_draw();
-        self.gpu_context.image_buffer_to_screen(&mut encoder);
 
-        self.ui.text("welcome_message", "hello world");
+        self.ui.text("fps", &(1f32 / dt).to_string());
 
         self.ui.draw(
             &self.gpu_context,
             &mut encoder,
             window,
-            self.gpu_context.surface_texture_view.as_ref().unwrap(),
             input.mouse_state.mouse_location,
             input.mouse_state.get_left_button() == ButtonState::Pressed,
         );
@@ -47,7 +40,7 @@ impl Game for HelloWorld {
         RenderResult::Continue
     }
 
-    fn on_resize(&mut self, physical_size: PhysicalSize<u32>) {}
+    fn on_resize(&mut self, _new_size: (u32, u32)) {}
 
     fn on_tick(&mut self, _dt: f32) -> UpdateResult {
         UpdateResult::Continue
