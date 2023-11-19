88c0e9784e56464e084d0fcc5bb5ba2767932235
rosaliedewinther <rosaliedewinther@gmail.com>
2023-11-19 12:48:10 +0100

    worked on atmosphere

--- examples/voxel_tracer/shaders/trace.glsl
+++ examples/voxel_tracer/shaders/trace.glsl
@@ -9,72 +9,108 @@ buffer trace_data{
     uvec2 screen_dimensions;
 };
 
-bool intersect(vec3 origin, vec3 direction, vec3 center, float radius2, out float t) {
-        float t0, t1; // solutions for t if the ray intersects
-        // geometric solution
-        vec3 L = center - origin;
-        float tca = dot(L, direction);
-        // if (tca < 0) return false;
-        float d2 = dot(L, L) - tca * tca;
-        if (d2 > radius2) return false;
-        float thc = sqrt(radius2 - d2);
-        t0 = tca - thc;
-        t1 = tca + thc;
-        if (t0 > t1) {
-            float temp = t0;
-            t0 = t1;
-            t1 = temp;
-        }
+const float atmosphereRadius = 1500;
+const float planetRadius = 800;
+const float sunRadius = 0.99;
 
+vec2 intersect(vec3 origin, vec3 direction, vec3 center, float radius2) {
+    float t0, t1; // solutions for t if the ray intersects
+    // geometric solution
+    vec3 L = center - origin;
+    float tca = dot(L, direction);
+    // if (tca < 0) return false;
+    float d2 = dot(L, L) - tca * tca;
+    if (d2 > radius2) return vec2(-1,-1);
+    float thc = sqrt(radius2 - d2);
+    t0 = tca - thc;
+    t1 = tca + thc;
+    if (t0 > t1) {
+        float temp = t0;
+        t0 = t1;
+        t1 = temp;
+    }
 
-        if (t0 < 0) {
-            t0 = t1; // if t0 is negative, let's use t1 instead
-            if (t0 < 0) return false; // both t0 and t1 are negative
-        }
-
-        t = t0;
-
-        return true;
+    return vec2(t0, t1);
 }
 
 vec3 mie(float dist, vec3 sunL){
     return max(exp(-pow(dist, 0.25)) * sunL - 0.4, 0.0);
 }
 
-vec3 getSky(vec3 rayDir){
-    vec3 sundir = normalize( vec3(cos(iTime), sin(iTime), 0.) );
-    
-    float yd = min(rayDir.y, 0.);
-    rayDir.y = max(rayDir.y, 0.);
-    
-    vec3 col = vec3(0.);
-    
-    col += vec3(.4, .4 - exp( -rayDir.y*20. )*.15, .0) * exp(-rayDir.y*9.); // Red / Green 
-    col += vec3(.3, .5, .6) * (1. - exp(-rayDir.y*8.) ) * exp(-rayDir.y*.9) ; // Blue
-    
-    col = mix(col*1.2, vec3(.3),  1.-exp(yd*100.)); // Fog
-    
-    col += vec3(1.0, .8, .55) * pow( max(dot(rayDir,sundir),0.), 15. ) * .6; // Sun
-    col += pow(max(dot(rayDir, sundir),0.), 150.0) *.15;
-    
-    return col;
+float getSkyDensity(vec3 position){
+    // distance from ground` 
+    return (1-exp(-((dot(position, position) - planetRadius)/(atmosphereRadius-planetRadius))));
 }
+
+float stepSize(inout vec3 origin, vec3 direction, vec2 intersection_results, uint steps){
+    if (intersection_results.x >= 0){
+        origin += direction*(intersection_results.x+0.0001);
+    }
+    return (intersection_results.y - max(intersection_results.x, 0.0)) / steps;
+}
+
+const uint steps = 32;
+const uint shadow_steps = 8;
+
+float getSunOpticalDepth(vec3 origin, vec3 sunDir){
+    float opticalDepth = 0;
+    vec2 intersection_results = intersect(origin, sunDir, vec3(0), atmosphereRadius);
+    float step_size = stepSize(origin, sunDir, intersection_results, shadow_steps);
+    for (int i = 1; i < shadow_steps; i++){
+        float density = getSkyDensity(origin + i*step_size*sunDir);
+        opticalDepth += density * step_size;
+    }
+    return opticalDepth;
+}
+
+float getSun(vec3 direction, vec3 sunDir){
+    if (sunRadius < dot(direction, sunDir)){
+        return 1.0;
+    } else {
+        return 0.0;
+    }
+}
+
+vec3 getSky(vec3 origin, vec3 direction){
+    vec3 sundir = normalize( vec3(cos(iTime/50), sin(iTime/50), 0.) );
+    vec2 intersection_results = intersect(origin, direction, vec3(0), atmosphereRadius);
+    float step_size = stepSize(origin, direction, intersection_results, steps);
+    float opticalDepth = 0;
+    vec3 light = vec3(0,0,0);
+    
+    if (intersection_results.y >= 0){
+        for (int i = 1; i < steps; i++){
+            vec3 samplePos = origin + direction * step_size * i;
+            float density = getSkyDensity(samplePos);
+            opticalDepth += density * step_size;
+            float sunOpticalDepth = getSunOpticalDepth(samplePos, sundir);
+            light += density*density * exp(-(opticalDepth + sunOpticalDepth))*skylight;
+        }
+    }
+
+    light += vec3(1) * getSun(direction, sundir) * exp(-opticalDepth); 
+    
+    
+    return vec3(light);
+}
+
+
 
 void main()
 {
     ivec2 pos = ivec2(gl_GlobalInvocationID.xy);
     vec3 direction = imageLoad(primary_ray_direction, pos).xyz;
     vec4 color;
-    float t;
-    vec3 ball_pos = vec3(1,0,0);
-    if (intersect(camera_pos, direction, ball_pos, 1.0, t)){
-        vec3 hit_pos = camera_pos + t*direction;
+    vec3 ball_pos = vec3(0,0,0);
+    vec2 intersection_results = intersect(camera_pos, direction, ball_pos, planetRadius);
+    if (intersection_results.x >= 0 && intersection_results.y >= 0){
+        vec3 hit_pos = camera_pos + intersection_results.x*direction;
         vec3 new_dir = reflect(direction, normalize(hit_pos - ball_pos));
-        color = vec4(getSky(new_dir)* vec3(1,1,0.5), 1.0);
+        color = vec4(getSky(hit_pos, new_dir)* vec3(1,1,0.5), 1.0);
     } else {
-        color = vec4(getSky(direction), 1.0);
+        color = vec4(getSky(camera_pos, direction), 1.0);
     }
 
-	color = color / (2.0 * color + 0.5 - color);
+	//color = color / (2.0 * color + 0.5 - color);
     imageStore(screen_texture, pos, color);
 }
